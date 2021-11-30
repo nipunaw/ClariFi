22b40ff2c1a9a2ec4dd981d33b465fe052303ce3
Leslie Harvey <leslie.harvey@ufl.edu>
2021-11-30 14:19:30 -0500

    integrate working solution with react

--- public/main.js
+++ public/main.js
@@ -1,29 +1,50 @@
-const electron = require("electron");
-const url = require("url");
-const path = require("path");
+const { app, BrowserWindow, Menu } = require("electron");
 
-const { app, BrowserWindow, Menu } = electron;
-
-let mainWindow;
-
-app.on("ready", function () {
-  //Wait for app to be ready
-  mainWindow = new BrowserWindow({}); //Create browser window
-  mainWindow.loadURL(
-    url.format({
-      //Load HTML
-      pathname: path.join(__dirname, "index.html"),
-      protocol: "file:",
-      slashes: true,
-    })
-  );
-  mainWindow.on("closed", function () {
-    // Main window quitting = global quitting
-    app.quit();
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
   });
+
+  //load the index.html from a url
+  win.loadURL("http://localhost:3000");
+
+  // Open the DevTools.
+  win.webContents.openDevTools();
+
   const mainMenu = Menu.buildFromTemplate(mainMenuTemplate); //Set menu
   Menu.setApplicationMenu(mainMenu);
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
+
+app.on("activate", () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
 
 //Main Menu Template
 const mainMenuTemplate = [
