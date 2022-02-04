60a10ee572c9c74a4f08241b4a9bc0190810f96d
Nipuna Weerapperuma <nipunaw12@gmail.com>
2022-02-03 20:08:00 -0500

    FIR Filter coefficients (lowpass example), modularized noise removal

--- public/main/audioProcess.js
+++ public/main/audioProcess.js
@@ -3,34 +3,64 @@ const Pitchfinder = require("pitchfinder");
 const fft = require('fft-js').fft;
 const fftUtil = require('fft-js').util;
 const windowing = require('fft-windowing');
+const Fili = require('fili');
 
 const fftAnalysis = (rawRecordedData, sampleRate) => {
-    var windowedRecordedData = windowing.hann(rawRecordedData);
-    var phasors = fft(windowedRecordedData.slice(0, 262144)); // 48000 > samples 
-    var frequencies = fftUtil.fftFreq(phasors, sampleRate); // Sample rate and coef is just used for length, and frequency step
-    var magnitudes = fftUtil.fftMag(phasors);
+  var windowedRecordedData = windowing.hann(rawRecordedData);
+  var phasors = fft(windowedRecordedData.slice(0, 262144)); // 48000 > samples 
+  var frequencies = fftUtil.fftFreq(phasors, sampleRate); // Sample rate and coef is just used for length, and frequency step
+  var magnitudes = fftUtil.fftMag(phasors);
 
-    console.log("Sample Rate: " + sampleRate);
-    console.log("Length of raw data: " + rawRecordedData.length);
-    console.log("Length of windowed data: " + windowedRecordedData.length);
-    console.log("Length of phasors: " + phasors.length);
-    console.log("Length of frequencies: " + frequencies.length);
-    console.log("Length of magnitudes: " + magnitudes.length);
-    console.log("");
-    firFilterTaps(frequencies, magnitudes)
-
+  console.log("Sample Rate: " + sampleRate);
+  console.log("Length of raw data: " + rawRecordedData.length);
+  console.log("Length of windowed data: " + windowedRecordedData.length);
+  console.log("Length of phasors: " + phasors.length);
+  console.log("Length of frequencies: " + frequencies.length);
+  console.log("Length of magnitudes: " + magnitudes.length);
+  console.log("");
+  firFilterTaps(frequencies, magnitudes);
 }
 
 const firFilterTaps = (frequencies, magnitudes) => {
-    const magnitudesDescending = [...magnitudes].sort(function (a, b) {  return a - b;  }).reverse();
-    const magnitudesThreshold = magnitudesDescending[20];
+  noiseRemoval(frequencies, magnitudes);
+  var firCalculator = new Fili.FirCoeffs();
+  var firFilterCoeffs = firCalculator.lowpass({
+    order: 100, // filter order
+    Fs: 1000, // sampling frequency
+    Fc: 100 // cutoff frequency
+    // forbandpass and bandstop F1 and F2 must be provided instead of Fc
+  });
+  console.log(firFilterCoeffs);
+}
 
-    for (let i = 0; i < frequencies.length; i++) {
-      if (magnitudes[i] > magnitudesThreshold) {
-        console.log("Frequency (Hz): "+ frequencies[i] + ", Magnitude: "+ magnitudes[i])
-      }
+const noiseRemoval = (frequencies, magnitudes) => {
+  let noiseLength = 0;
+  for (let i = 0; i < frequencies.length; i++) {
+    if (frequencies[i] < 150) {
+      noiseLength++;
     }
-    console.log("");
+  }
+  const magnitudesNoiseDescending = [...magnitudes.slice(0, noiseLength)].sort(function (a, b) {  return a - b;  }).reverse();
+  const magnitudesNoiseThreshold = magnitudesNoiseDescending[20];
+  for (let i = 0; i < noiseLength; i++) {
+    if (magnitudes[i] > magnitudesNoiseThreshold) {
+      console.log("Noise Frequency (Hz): "+ frequencies[i] + ", Noise Magnitude: "+ magnitudes[i])
+    }
+  }
+  console.log("");
+
+}
+
+const generalAnalysis = (frequencies, magnitudes) => {
+  const magnitudesDescending = [...magnitudes].sort(function (a, b) {  return a - b;  }).reverse();
+  const magnitudesThreshold = magnitudesDescending[15];
+
+  for (let i = 0; i < frequencies.length; i++) {
+    if (magnitudes[i] > magnitudesThreshold) {
+      console.log("General Frequency (Hz): "+ frequencies[i] + ", Noise Magnitude: "+ magnitudes[i])
+    }
+  }
+  console.log("");
 }
 
 const GetPitchValue = (recordedData) => {
