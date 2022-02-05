214c631515ea4e007a620e94bc860d1b4292631a
Leslie Harvey <leslie.harvey@ufl.edu>
2022-02-04 23:05:29 -0500

    update word break

--- src/components/NoiseInfoPrompt.tsx
+++ src/components/NoiseInfoPrompt.tsx
@@ -4,9 +4,14 @@ const NoiseInfoPrompt: React.FC<{
   advanceState: () => any;
 }> = ({ advanceState }) => {
   return (
-    <div className="main-content">
+    <div className="main-content" style={{ wordBreak: "break-word" }}>
       <div className="display-message">Noise Test</div>
-      <div style={{ marginTop: "15px", marginBottom: "20px" }}>
+      <div
+        style={{
+          marginTop: "15px",
+          marginBottom: "20px",
+        }}
+      >
         This test is going to assess the ambient noise in your current
         enviornment. Please try to replicate the expected background noise.
       </div>
