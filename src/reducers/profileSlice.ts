f6117955bf9c97959b94f0c33141a68f2ed05920
Leslie Harvey <leslie.harvey@ufl.edu>
2022-04-05 16:02:53 -0400

    add profile redux

--- src/reducers/profileSlice.ts
+++ src/reducers/profileSlice.ts
@@ -1,26 +1,38 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Profile } from "types/profile";
 import type { RootState } from "../store";
 
 interface ProfileState {
-  currentProfileId: string | null;
+  currentProfileId: number | null;
+  nextProfileId: number;
+  savedProfiles: Profile[];
 }
 
 const initialState: ProfileState = {
   currentProfileId: null,
+  nextProfileId: 0,
+  savedProfiles: [],
 };
 
 export const profileSlice = createSlice({
   name: "profile",
   initialState,
   reducers: {
-    setProfile: (state, action: PayloadAction<number>) => {},
+    setProfile: (state, action: PayloadAction<number>) => {
+      state.currentProfileId = action.payload;
+    },
+    createProfile: (state, action: PayloadAction<Profile>) => {
+      state.savedProfiles.push(action.payload);
+      state.nextProfileId = state.nextProfileId + 1;
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { setProfile } = profileSlice.actions;
+export const { setProfile, createProfile } = profileSlice.actions;
 
 // Other code such as selectors can use the imported `RootState` type
-//export const selectPaneTabArray = (state: RootState) => state.profileSlice.tabArray;
+export const selectProfileId = (state: RootState) =>
+  state.profile.currentProfileId;
 
 export default profileSlice.reducer;
---
+++ src/types/profile.ts
@@ -0,0 +1,5 @@
+export interface Profile {
+  id: Number;
+  name: String;
+  values: Number[];
+}
