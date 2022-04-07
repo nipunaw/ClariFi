f81474f6de3f3b4dc8f0efae2be8c68d1a4bc52a
Nipuna Weerapperuma <nipunaw12@gmail.com>
2022-04-06 21:02:27 -0400

    Updating user application icon

--- public/electron.js
+++ public/electron.js
@@ -17,6 +17,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1000,
     height: 800,
+    icon: __dirname + "/favicon.ico",
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
